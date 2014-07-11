aa648a4876fedc08e329615f0f2e2df971c1e5ec
Tim Burke <timburke@alumni.rice.edu>
2014-07-11 13:04:23 -0700

    Bug fixes and add Cmd library

--- test_i2c.ino
+++ test_i2c.ino
@@ -19,12 +19,14 @@ void setup()
  cmdAdd("sub2no", sub_to_no);
  cmdAdd("pin2no", pin_to_no);
  cmdAdd("initstatus", setup_status);
+ cmdAdd("status", report_status);
+ cmdAdd("substatus", report_substrate);
  
  //Disconnect all substrates from NO pin
- for (i=1; i<40; i+=1)
+ for (i=1; i<=40; i+=1)
  {
    uint8_t addr = map_substrate(i);
-   uint8_t err;
+   uint8_t err=0;
    
    err = init_substrate(addr, 0);
    
@@ -37,6 +39,80 @@ void setup()
  
  setup_done = 1;
  pformat("Done initializing substrates");
+}
+
+void report_substrate(int argc, char **argv)
+{
+  if (argc != 2)
+  {
+    pformat("Usage: substatus <substrate #1-40>");
+    return;
+  }
+  
+    uint8_t addr = map_substrate(cmdStr2Num(argv[1],10));
+    uint16_t dir;
+    uint16_t lev;
+    uint16_t no_pins;
+    
+    lev = get_level(addr);
+    dir = get_direction(addr);
+    
+    pformat("Substrate %s status", argv[1]);
+    pformat("Address: 0x%X", addr);
+    pformat("Direction: 0x%X", dir);
+    pformat("Levels: 0x%X", lev);
+}
+
+void report_status(int argc, char **argv)
+{
+  int i, j;
+  for (i = 1; i <= 40; i+=1)
+  {
+    uint8_t addr = map_substrate(i);
+    uint8_t err;
+    uint16_t dir;
+    uint16_t lev;
+    uint16_t no_pins;
+    
+    lev = get_level(addr);
+    dir = get_direction(addr);
+    
+    if (lev == 0xAAAA && dir == 0xAAAA)
+    {
+      pformat("Substrate %d: IS NOT RESPONDING", i);
+      continue;
+    }
+    else if(lev & 0xFF == 0xAA || lev >> 8 == 0xAA || dir & 0xFF == 0xAA || lev >> 8 == 0xAA)
+    {
+      pformat("Substrate %d: error reading registers, but substrate did respond", i);
+      continue;
+    }
+    
+    //Print out what we got
+    //Do not report substrates that are all connected to normally closed as they should be
+    if (dir == 0 && lev == 0)
+    {
+      continue;
+    }
+    
+    if (dir != 0)
+    {
+      pformat("Substrate %d: not all pins were configured as outputs.  This is an error.", i);
+    }
+   
+    no_pins = ~(lev | dir);
+    
+    for (j=0; j<10; j+=1)
+    {
+      uint16_t probe = 1 << j;
+      if (no_pins & probe)
+        pformat("Substrate %d: pin index %d connected to NO", i, j);
+        
+        no_pins >>= 1;
+    }
+  }
+  
+  pformat("All other substrates have all pins connected to normally closed."); 
 }
 
 void pformat(char *fmt, ... )
@@ -61,7 +137,7 @@ void sub_to_no(int argc, char **argv)
   uint32_t addr;
   uint8_t err;
   
-  if (argc < 3)
+  if (argc != 3)
   {
     Serial.println("usage: sub2no <substrate> <yes|no>");
     return;
@@ -102,7 +178,7 @@ void pin_to_no(int argc, char **argv)
   uint8_t pin, result;
   uint32_t addr;
   
-  if (argc < 3)
+  if (argc != 3)
   {
     Serial.println("usage: pin2no <substrate> <pin>");
     return;
@@ -117,9 +193,9 @@ void pin_to_no(int argc, char **argv)
   addr = map_substrate(cmdStr2Num(argv[1],10));
   
   if (result == 0)
-    pformat("Setting Pin %d @ Address 0x%x to NO", pin, addr);
+    pformat("Setting Pin %d @ Address 0x%x to NO", pin, addr << 1);
   else
-    pformat("Invalid Pin or Substrate Indices: pin %s at substrate %s", argv[2], argv[1]);
+    pformat("Invalid Pin or Substrate Indices: arguments were pin '%s', substrate '%s'", argv[2], argv[1]);
 }
 
 void loop()
